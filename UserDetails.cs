20b4adf2b0f84b907fdbad28f445143c0d3b045e
RourkeV <111749734+RourkeV@users.noreply.github.com>
2024-08-19 17:19:53 +0200

    filled list and added age

--- UserDetails.cs
+++ UserDetails.cs
@@ -12,16 +12,19 @@ namespace PRG281_Project
         private string name;
         private string email;
         private string password;
+        private int age;
 
         public string name1 { get => name; set => name = value; }
         public string email1 { get => email; set => email = value; }
         public string password1 { get => password; set => password = value; }
+        public int age1 { get => age; set => age = value; }
 
-        public UserDetails(string name, string email, string password)
+        public UserDetails(string name, string email, string password, int age)
         {
             this.name1 = name;
             this.email1 = email;
             this.password1 = password;
+            this.age1 = age;
         }
     }
 }
