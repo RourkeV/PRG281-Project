79761f87c40226265d919b658e6f454f2c41d5ac
RourkeV <111749734+RourkeV@users.noreply.github.com>
2024-08-20 09:52:35 +0200

    dunno

--- UserSIgnUp.cs
+++ UserSIgnUp.cs
@@ -12,14 +12,21 @@ namespace PRG281_Project
 {
     public partial class UserSignUp : Form
     {
+        public static UserSignUp instance;
+        public TextBox email;
         public UserSignUp()
         {
             InitializeComponent();
+            instance = this;
+            email = textBox1;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            //need to add validation
+            string userEmail = textBox1.Text;
+            string userPass = textBox2.Text;
+            textBox1.Clear();
         }
 
         private void label1_Click(object sender, EventArgs e)
