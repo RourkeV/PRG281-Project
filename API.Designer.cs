e6af1e1f52e73a76057df15f6f1e09c47ec2492e
FreeYungHammy <calvin@nijenhuis.life>
2024-08-20 20:14:49 +0200

    Form Adjustments

--- API.Designer.cs
+++ API.Designer.cs
@@ -36,11 +36,13 @@
             // 
             // textBox
             // 
+            this.textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox.Location = new System.Drawing.Point(63, 455);
             this.textBox.Multiline = true;
             this.textBox.Name = "textBox";
             this.textBox.Size = new System.Drawing.Size(268, 46);
             this.textBox.TabIndex = 0;
+            this.textBox.Text = "Enter Your Message...";
             this.textBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // richTextBox
@@ -50,24 +52,25 @@
             this.richTextBox.ReadOnly = true;
             this.richTextBox.Size = new System.Drawing.Size(370, 354);
             this.richTextBox.TabIndex = 1;
-            this.richTextBox.Text = "";
+            this.richTextBox.Text = "Your conversation will appear here...";
             // 
             // submitBtn
             // 
             this.submitBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.submitBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.submitBtn.Location = new System.Drawing.Point(346, 455);
             this.submitBtn.Name = "submitBtn";
             this.submitBtn.Size = new System.Drawing.Size(95, 46);
             this.submitBtn.TabIndex = 2;
-            this.submitBtn.Text = "----------->";
+            this.submitBtn.Text = "Send";
             this.submitBtn.UseVisualStyleBackColor = true;
             // 
             // lblHeading
             // 
             this.lblHeading.AutoSize = true;
-            this.lblHeading.BackColor = System.Drawing.Color.White;
+            this.lblHeading.BackColor = System.Drawing.Color.RosyBrown;
             this.lblHeading.Font = new System.Drawing.Font("PMingLiU-ExtB", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblHeading.Location = new System.Drawing.Point(59, 35);
+            this.lblHeading.Location = new System.Drawing.Point(59, 48);
             this.lblHeading.Name = "lblHeading";
             this.lblHeading.Size = new System.Drawing.Size(223, 20);
             this.lblHeading.TabIndex = 3;
@@ -75,6 +78,7 @@
             // 
             // API
             // 
+            this.BackColor = System.Drawing.Color.RosyBrown;
             this.ClientSize = new System.Drawing.Size(504, 557);
             this.Controls.Add(this.lblHeading);
             this.Controls.Add(this.submitBtn);
