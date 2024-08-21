b2246696a7643ecfecf7652cd03fb31fa8b72513
Rourke Veller <rourke@nitida.co.za>
2024-08-21 15:50:40 +0200

    changing global list

---
+++ UserSignIn.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PRG281_Project
+{
+    public partial class UserSignIn : Form
+    {
+        List<UserDetails> users = new List<UserDetails>();
+        public UserSignIn()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            
+            string email;
+            string password;
+            email = txtEmail.Text;
+            password = txtPass.Text;
+
+            //need to add validation and checks
+
+            //AddingDetails detailPage = new AddingDetails(); ***this page needs to link to swipe page
+            //detailPage.Show();
+            Visible = false;
+            txtEmail.Clear();
+        }
+
+        private void UserSignIn_Load(object sender, EventArgs e)
+        {
+            FullUserList fullUserList = new FullUserList();
+            users = fullUserList.AllUsers;
+        }
+    }
+}
